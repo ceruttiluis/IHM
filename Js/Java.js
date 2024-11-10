8516de83356c563b7e68e86a3438a99f429c57d4
Cerutti <luissantimcerutti@hotmail.com>
2024-11-10 19:09:27 -0300

    second commit

--- Js/Java.js
+++ Js/Java.js
@@ -1,3 +1,27 @@
+$(document).ready(function() {
+
+    let bannerAtual = 1;
+    let listaBanner = ["banner1", "banner2", "banner3"];
+
+    setInterval(mudarBanner, 2000);
+
+    function mudarBanner() {
+        if (bannerAtual > 0) { 
+            $("#carrossel").removeClass(listaBanner[bannerAtual - 1]);  
+        } else {
+            $("#carrossel").removeClass(listaBanner[2]);
+        }
+
+        $("#carrossel").addClass(listaBanner[bannerAtual]);
+        
+        bannerAtual++;
+
+        if (bannerAtual > 2) {
+            bannerAtual = 0;
+        }
+    }
+})
+
 function abrirMenu() {
     document.getElementById("menu").style.width = "350px";
 }
